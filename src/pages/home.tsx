757e067212dd975b11997057c2f18eed424648b5
smy-101 <shimy1011@foxmail.com>
2021-04-23 19:47:28 +0800

    完成chart1、chart2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './home.scss';
-import headerBg from '../images/header.png'
+import headerBg from '../images/header.png';
+import {Chart1} from '../components/chart-1';
+import {Chart2} from '../components/chart-2';
 
 export const Home = () => {
-  return (
-      <div className="home">
-          <header style={{backgroundImage: `url(${headerBg})`}}/>
-          <main>
-              <section className="section1"></section>
-              <section className="section2"></section>
-              <section className="section3"></section>
-              <section className="section4"></section>
-              <section className="section5"></section>
-          </main>
-      </div>
-  );
+
+    return (
+        <div className="home">
+            <header style={{backgroundImage: `url(${headerBg})`}}/>
+            <main>
+                <section className="section1">
+                    <Chart1/>
+                    <Chart2/>
+                </section>
+                <section className="bordered section2"></section>
+                <section className="bordered section3"></section>
+                <section className="bordered section4"></section>
+                <section className="bordered section5"></section>
+            </main>
+        </div>
+    );
 };
